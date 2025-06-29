4c5620d6ed4eb0416c2147c7c03c05d9583df51b
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-29 13:12:31 +0300

    created project, setup tailwind and config and creaded first components

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import Logo from "./Logo";
+
+function Header() {
+
+const navLinks = []
+
+  return (
+    <header>
+      <Logo />
+      <nav>
+        <ul></ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
