3fc3da94ca4f9dce8acee863a6d5b92d86bb5808
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-30 20:51:12 +0300

    updated app favicon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 function Button({ children }) {
-  return <button className="btn px-10 py-2 rounded-full">{children}</button>;
+  return <button className="btn px-[60px] py-2 rounded-full font-medium">{children}</button>;
 }
 
 export default Button;
