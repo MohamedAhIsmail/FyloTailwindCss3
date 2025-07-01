2f99d0a3967c15abd9974755858bc1fb7f83697f
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-07-01 20:13:16 +0300

    refactor footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,43 @@ import ToTop from "./ToTop";
 import iconLocation from "../assets/icon-location.svg";
 import iconEmail from "../assets/icon-email.svg";
 import iconPhone from "../assets/icon-phone.svg";
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
+
+const social = [
+  {
+    icon: <FaFacebookF />,
+  },
+  {
+    icon: <FaTwitter />,
+  },
+  {
+    icon: <FaInstagram />,
+  },
+];
+const linksOne = ["About us", "Jobs", "Press", "Blog"];
+const linksTwo = ["Contact us", "Terms", "Privacy"];
+const info = [
+  {
+    image: iconPhone,
+    name: "+1-543-123-4567",
+  },
+  {
+    image: iconEmail,
+    name: "example@email.com",
+  },
+];
 
 function Footer() {
   return (
     <>
       <footer className="bg-[#0C1524] text-white pt-72 pb-40">
-        <div className="container grid grid-cols-1 gap-10 place-items-center md:grid-cols-2 lg:grid-cols-4">
+        <div className="container grid grid-cols-1 gap-10 place-items-start md:place-items-center md:grid-cols-2 lg:grid-cols-4">
           <div>
             <Logo />
             <div className="flex items-start gap-5 mt-8">
               <img src={iconLocation} alt="Location" />
-              <p>
+              <p className="text-sm">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel,
                 labore quis ea nam totam corrupti amet, ipsam nesciunt
                 teneturfacilis sit
@@ -25,62 +48,43 @@ function Footer() {
           </div>
 
           <div className="flex flex-col gap-5">
-            <div className="flex gap-5 items-center">
-              <img src={iconPhone} alt="Phone" className="" />
-              <p>+1-543-123-4567</p>
-            </div>
-
-            <div className="flex gap-5 items-center">
-              <img src={iconEmail} alt="Email" />
-              <p>example@email.com</p>
-            </div>
+            {info.map((item, i) => (
+              <div key={i} className="flex gap-5 items-center">
+                <img src={item.image} alt="Phone" />
+                <p>{item.name}</p>
+              </div>
+            ))}
           </div>
 
-          <div className="flex justify-between gap-20">
+          <div className="flex flex-col gap-10 md:flex-row md:gap-20">
             <ul>
-              <li>
-                <a href="#">About us</a>
-              </li>
-              <li>
-                <a href="#">Jobs</a>
-              </li>
-              <li>
-                <a href="#">Press</a>
-              </li>
-              <li>
-                <a href="#">Blog</a>
-              </li>
+              {linksOne.map((link, i) => (
+                <li key={i}>
+                  <a href="#">{link}</a>
+                </li>
+              ))}
             </ul>
             <ul>
-              <li>
-                <a href="#">Contact us</a>
-              </li>
-              <li>
-                <a href="#">Terms</a>
-              </li>
-              <li>
-                <a href="#">Privacy</a>
-              </li>
+              {linksTwo.map((link, i) => (
+                <li key={i}>
+                  <a href="#">{link}</a>
+                </li>
+              ))}
             </ul>
           </div>
 
-          <div>
+          <div className="mx-auto">
             <ul className="flex gap-5">
-              <li className="w-10 h-10 rounded-full flex items-center justify-center border-[1px]">
-                <a href="#">
-                  <FaFacebookF />
-                </a>
-              </li>
-              <li className="w-10 h-10 rounded-full flex items-center justify-center border-[1px]">
-                <a href="#">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li className="w-10 h-10 rounded-full flex items-center justify-center border-[1px]">
-                <a href="#">
-                  <FaInstagram />
-                </a>
-              </li>
+              {social.map((link, i) => (
+                <li key={i}>
+                  <a
+                    href="#"
+                    className="w-10 h-10 rounded-full flex items-center justify-center border-[1px] hover:text-[#4EC6D4] transition-all duration-200"
+                  >
+                    {link.icon}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
